beae2cc5f8be88859ac718584cdb2dc69ff92e6e
Lucas <l.britzelli@gmail.com>
2025-08-04 01:22:06 +0200

    Switched logging method to notify that a baby zombie has been deleted.

--- src/main/java/com/babyzombieremoval/BabyZombieRemoval.java
+++ src/main/java/com/babyzombieremoval/BabyZombieRemoval.java
@@ -6,7 +6,13 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.util.TypeFilter;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+
 public class BabyZombieRemoval implements ModInitializer {
+	public static final Logger LOGGER = LoggerFactory.getLogger("BabyZombieRemoval");
 	@Override
 	public void onInitialize() {
 		ServerTickEvents.END_WORLD_TICK.register(world -> {
@@ -15,7 +21,7 @@ public class BabyZombieRemoval implements ModInitializer {
 					ZombieEntity::isBaby
 			)) {
 				zombie.remove(Entity.RemovalReason.DISCARDED);
-				System.out.println("[BabyZombieRemoval] Deleted a baby zombie.");
+				LOGGER.info("Deleted a baby zombie.");
 			}
 		});
 	}
