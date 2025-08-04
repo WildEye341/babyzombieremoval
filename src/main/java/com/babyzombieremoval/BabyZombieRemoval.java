6b2bd0537f9784958823fafc346740dea3f186cd
Lucas <l.britzelli@gmail.com>
2025-08-04 02:31:19 +0200

    attempted to fix console logging whenever a baby zombie gets deleted (unsuccessfully) and also made the mod use a proper icon

--- src/main/java/com/babyzombieremoval/BabyZombieRemoval.java
+++ src/main/java/com/babyzombieremoval/BabyZombieRemoval.java
@@ -6,22 +6,24 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.ZombieEntity;
 import net.minecraft.util.TypeFilter;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 
 
 public class BabyZombieRemoval implements ModInitializer {
-	public static final Logger LOGGER = LoggerFactory.getLogger("BabyZombieRemoval");
+	public static final Logger LOGGER = LogManager.getRootLogger();
 	@Override
 	public void onInitialize() {
+		LogManager.getRootLogger().info("[BabyZombieRemoval] initialized");
 		ServerTickEvents.END_WORLD_TICK.register(world -> {
 			for (ZombieEntity zombie : world.getEntitiesByType(
 					TypeFilter.instanceOf(ZombieEntity.class),
 					ZombieEntity::isBaby
 			)) {
 				zombie.remove(Entity.RemovalReason.DISCARDED);
-				LOGGER.info("Deleted a baby zombie.");
+				System.out.println("[BabyZombieRemoval] has deleted a baby zombie.");
+				LogManager.getRootLogger().warn("[BabyZombieRemoval] has deleted a baby zombie.");
 			}
 		});
 	}
